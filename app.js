cba99e50355c20175bd2ca0897c31a6aa8d92602
ritasahaa <ritasah1602@gmail.com>
2024-03-30 19:10:06 +0530

    Basic API/route created

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello rita! get')
+})
+
+app.post('/', (req, res) => {
+  res.send('Hello rita! post')
+})
+
+app.put('/', (req, res) => {
+  res.send('Hello rita! put')
+})
+
+app.delete('/', (req, res) => {
+  res.send('Hello rita! delete')
+})
 
 app.listen(port, () => {
   console.log(`web app listening on port ${port}`);
